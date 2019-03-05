e0fe756a60e828ef6f941f3be4a7a51f71b8de95
Alex Vihlayew <vihlayew.alex@gmail.com>
2019-03-06 00:07:34 +0200

    Initial Commit

---
+++ KPI Schedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KPI Schedule
+//
+//  Created by Alex Vihlayew on 3/6/19.
+//  Copyright © 2019 Oleksandr Vikhliaiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
